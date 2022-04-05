551fb6b8a02dbb03e26942f61b8d7e61c18ffb52
Ricardo Almeida <ricardoalmeida.for@gmail.com>
2022-04-05 16:13:36 -0300

    Initial Commit With Eureka Server and Gateway Cloud

---
+++ auth-service/src/main/java/com/qwertcardo/authservice/controllers/AuthController.java
@@ -0,0 +1,17 @@
+package com.qwertcardo.authservice.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class AuthController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body("Return with I Am A TEAPOT");
+    }
+}
